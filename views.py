e2703e90648e6acdb4bde72da8b2bb56e1897c3b
meghasn <41339621+meghasn@users.noreply.github.com>
2019-09-09 17:27:52 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+
+def homepage(request):
+    return render(request,'home.html')
+
+def count(request):
+    fulltext=  request.GET['fulltext']
+    wordlist=fulltext.split()
+    worddict={}
+    for word in wordlist:
+        if word in worddict:
+            worddict[word]+=1
+        else:
+            worddict[word]=1
+    sorwords=sorted(worddict.items(),key=operator.itemgetter(1),reverse=True)
+
+    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'worddict':sorwords})
